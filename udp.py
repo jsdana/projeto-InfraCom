8343ada99d07ffcf7d9074e245638f6a93603f32
JJ <julnsilva20@gmail.com>
2024-06-08 13:55:08 -0300

    (Tentativa de Entrega 1): O cliente irá enviar um arquivo para o servidor e esse irá armazená-lo e devolvê-lo.

---
+++ udp.py
@@ -0,0 +1,24 @@
+import socket as skt
+import time
+import os
+import socket as skt 
+import sys
+
+MAX_BUFF = 1024
+
+class UDP:
+    def __init__(self, skct_family, skct_type, skct_biding, max_buff=MAX_BUFF):
+        self.skct = skt.socket(skct_family, skct_type)
+        self.skct.bind(skct_biding)
+        self.skct.settimeout(10)
+        self.client_address = None
+        self.MAX_BUFF = max_buff
+
+    def listen(self):
+        while True:
+            data, origin = self.skct.recvfrom(self.MAX_BUFF)
+            yield data, origin
+
+    def send(self, server_addr: tuple[str, int], msg: bytes):
+        self.skct.sendto(msg, server_addr)
+        time.sleep(0.0001)
